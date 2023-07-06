9dca0d2067502f2454258bbee2c0625c083c8fac
Matthew Yang <matthewyang434@gmail.com>
2023-07-05 19:09:42 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+//['Antonia', 'Nuru', 'Amari', 'Mo'] drivers names
+const returnFirstTwoDrivers = function(drivers) {    
+    const firstDrivers = drivers.slice(0, 2)
+    return firstDrivers;
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    const lastDrivers = drivers.slice(2, 4)
+    return lastDrivers;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(fare) {
+    return (function() {
+        fare = fare * fare;
+        return fare;
+    }) 
+}
+
+const fareDoubler = function(createFareMultiplier) {
+    let fare = createFareMultiplier * 2 
+    return fare
+}
+
+const fareTripler = function(createFareMultiplier) {
+    let fare = createFareMultiplier * 3
+    return fare
+} 
+
+const selectDifferentDrivers = function(drivers, myFunction) { 
+    if (myFunction === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers)
+    } 
+    else {
+        return returnLastTwoDrivers(drivers)
+    }
+}
